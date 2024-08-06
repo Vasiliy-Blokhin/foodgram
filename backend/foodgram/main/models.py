0759e806ff6f6aa3afd7b6d660128968c1eb966a
Vasiliy-Blokhin <Vasiliyblo97@gmail.com>
2024-08-06 21:57:45 +1000

    try to correct avatar

--- backend/foodgram/main/models.py
+++ backend/foodgram/main/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-
 class User(AbstractUser):
     first_name = models.CharField(
         verbose_name='имя пользователя', max_length=150, null=True, blank=True)
