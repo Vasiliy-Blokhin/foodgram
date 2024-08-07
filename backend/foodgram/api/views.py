a4bfd5cf2ac66f3a7376f808b477f44c001fb988
Vasiliy-Blokhin <Vasiliyblo97@gmail.com>
2024-08-07 23:16:21 +1000

    correct errors

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -96,6 +96,7 @@ class ProfileViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(
+            {'avatar': 'change success.'},
             status=status.HTTP_200_OK
         )
 
