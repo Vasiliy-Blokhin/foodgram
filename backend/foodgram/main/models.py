96422fda9c13bff74935538c0c0dc35570b29322
Vasiliy-Blokhin <Vasiliyblo97@gmail.com>
2024-08-11 23:40:38 +1000

    correct

--- backend/foodgram/main/models.py
+++ backend/foodgram/main/models.py
@@ -45,13 +45,6 @@ class User(AbstractUser):
         unique=True
     )
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = [
-        'username',
-        'first_name',
-        'last_name'
-    ]
-
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
